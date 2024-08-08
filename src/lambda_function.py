9e3b220abf52e669c05e2468dd9f9f23fae19b6b
yuli <stremovsky@gmail.com>
2024-08-08 18:26:25 +0300

    new version

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -5,14 +5,40 @@ import psycopg2.extras
 import json
 import base64
 
+def remove_port(host):
+    parts = host.split(':')
+    return parts[0]
+
+def base64url_decode(input_str):
+    # Base64URL decoding
+    padding = '=' * (4 - len(input_str) % 4)
+    return base64.urlsafe_b64decode(input_str + padding)
+
+def parse_jwt(token):
+    # Split the JWT into its components
+    header_encoded, payload_encoded, signature_encoded = token.split('.')
+
+    # Decode the header and payload
+    header = base64url_decode(header_encoded).decode('utf-8')
+    payload = base64url_decode(payload_encoded).decode('utf-8')
+
+    # Convert to JSON
+    header_json = json.loads(header)
+    payload_json = json.loads(payload)
+
+    # Return payload
+    return payload_json
+
 def lambda_handler(event, context):
-    token = event['headers']['Authorization']
-    decoded_token = jwt.decode(token, options={"verify_signature": False})
-    tenant_id = decoded_token['custom:tenant_id']
+    tenant_id = 1
+    if 'headers' in event and event['headers'] is not None and 'Authorization' in event['headers']:
+        token = event['headers']['Authorization']
+        decoded_token = parse_jwt(token)
+        tenant_id = decoded_token['custom:tenant_id']
 
     client = boto3.client('rds')
     auth_token = client.generate_db_auth_token(
-        DBHostname=os.getenv('DB_HOST'),
+        DBHostname=remove_port(os.getenv('DB_HOST')),
         Port=5432,
         DBUsername=os.getenv('DB_USER'),
         Region=os.getenv('REGION')
@@ -25,9 +51,9 @@ def lambda_handler(event, context):
         host=os.getenv('DB_HOST'),
         sslmode='require'
     )
-    
-    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cursor.execute(f"SELECT * FROM data_table WHERE tenant_id = %s", (tenant_id,))
+    cur = conn.cursor()
+    cur.execute("SET app.current_tenant TO %s", (tenant_id,))
+    cur.execute("SELECT * FROM your_table_name;")
     records = cursor.fetchall()
     
     return {
