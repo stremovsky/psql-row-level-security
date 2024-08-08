80d5635bb3e05e8b1c43378e35f142f61a27dbee
yuli <stremovsky@gmail.com>
2024-08-08 16:57:48 +0300

    sve file

--- infra.tf
+++ infra.tf
@@ -78,7 +78,7 @@ resource "aws_lambda_function" "fetch_records" {
   function_name    = "fetchRecords"
   role             = aws_iam_role.lambda_role.arn
   handler          = "lambda_function.lambda_handler"
-  runtime          = "python3.9"
+  runtime          = "python3.10"
   environment {
     variables = {
       DB_HOST     = aws_db_instance.postgres.endpoint
