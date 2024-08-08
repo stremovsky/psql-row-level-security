a718e5bc135d9825f5a435f8fe7024d3f1441c9b
yuli <stremovsky@gmail.com>
2024-08-08 22:07:59 +0300

    create suers with custom tenant_id

---
+++ create-users.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USER_POOL_ID=$(terraform output -raw user_pool_id)
+
+if [[ -z "$USER_POOL_ID" ]]; then
+    echo "Failed to get USER_POOL_ID from terraform output"
+    exit
+fi
+
+aws cognito-idp admin-create-user \
+    --user-pool-id $USER_POOL_ID \
+    --username user1 \
+    --temporary-password "1qaz@WSX" \
+    --user-attributes Name=email,Value=user1@example.com Name=custom:tenant_id,Value=1
+
+aws cognito-idp admin-create-user \
+    --user-pool-id $USER_POOL_ID \
+    --username user2 \
+    --temporary-password "1qaz@WSX" \
+    --user-attributes Name=email,Value=user2@example.com Name=custom:tenant_id,Value=2
